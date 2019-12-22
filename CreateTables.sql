1134d2802be601661b72bc69979734d2b9b90c63
Mateusz Gwozdz <37940021+Gwoozdz@users.noreply.github.com>
2019-12-22 17:28:02 +0100

    Update CreateTables.sql

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,11 +1,6 @@
--- Products
-IF OBJECT_ID('dbo.Products', 'U') IS NOT NULL
-    DROP TABLE dbo.Products
-GO
-
--- Sellers
-IF OBJECT_ID('dbo.Sellers', 'U') IS NOT NULL
-    DROP TABLE dbo.Sellers
+-- OrderDetails
+IF OBJECT_ID('dbo.OrderDetails', 'U') IS NOT NULL
+    DROP TABLE dbo.OrderDetails
 GO
 
 -- Orders
@@ -13,25 +8,36 @@ IF OBJECT_ID('dbo.Orders', 'U') IS NOT NULL
     DROP TABLE dbo.Orders
 GO
 
+-- Products
+IF OBJECT_ID('dbo.Products', 'U') IS NOT NULL
+    DROP TABLE dbo.Products
+GO
+
 -- Customers
 IF OBJECT_ID('dbo.Customers', 'U') IS NOT NULL
     DROP TABLE dbo.Customers
+GO
+
+-- Sellers
+IF OBJECT_ID('dbo.Sellers', 'U') IS NOT NULL
+    DROP TABLE dbo.Sellers
 GO
 
 
 -- Sellers
 CREATE TABLE dbo.Sellers
     ( 
-        [SellerID] INT IDENTITY(1,1) PRIMARY KEY NOT NULL
-        , [SellerName] NVARCHAR(40) NOT NULL
+        [SellerID] INT IDENTITY(1,1)
         , CONSTRAINT PK_Sellers PRIMARY KEY (SellerID)
+        , [SellerName] NVARCHAR(40) NOT NULL
     )
 ;
 
 -- Customers
 CREATE TABLE dbo.Customers
     ( 
-        [CustomerID] INT IDENTITY(1,1) PRIMARY KEY NOT NULL
+        [CustomerID] INT IDENTITY(1,1)NOT NULL
+        , CONSTRAINT PK_Customers PRIMARY KEY (CustomerID) 
         , [CustomerName] NVARCHAR(40) NOT NULL
     )
 ;
@@ -39,10 +45,11 @@ CREATE TABLE dbo.Customers
 -- Products
 CREATE TABLE dbo.Products
     ( 
-        [ProductID] INT IDENTITY(1,1) PRIMARY KEY NOT NULL
+        [ProductID] INT IDENTITY(1,1) NOT NULL
+        , CONSTRAINT PK_Products PRIMARY KEY (ProductID) 
         , [ProductName] NVARCHAR(40) NOT NULL
         , [SellerID] INT
-        , CONSTRAINT FK_1 FOREIGN KEY (SellerID)
+        , CONSTRAINT FK_Products_Sellers FOREIGN KEY (SellerID)
           REFERENCES dbo.Sellers (SellerID)
           ON DELETE CASCADE
           ON UPDATE CASCADE
@@ -52,12 +59,26 @@ CREATE TABLE dbo.Products
 -- Orders
 CREATE TABLE dbo.Orders
     ( 
-        [OrderID] INT IDENTITY(1,1) PRIMARY KEY NOT NULL
+        [OrderID] INT IDENTITY(1,1) NOT NULL
+        , CONSTRAINT PK_Orders PRIMARY KEY (OrderID)
+        , [OrderDate] DATETIME NOT NULL
+        , [ShippedDate] DATETIME NOT NULL
+    )
+;
+
+-- Order Details
+CREATE TABLE dbo.OrderDetails
+    ( 
+        [OrderID] INT NOT NULL
+        , CONSTRAINT PK_OrderDetails PRIMARY KEY (OrderID)
+        , CONSTRAINT FK_OrderDetails_Orders FOREIGN KEY (OrderID)
+          REFERENCES dbo.Orders (OrderID)
+          ON DELETE CASCADE
+          ON UPDATE CASCADE
         , [ProductID] INT NOT NULL
-        , CONSTRAINT FK_Orders_Products FOREIGN KEY (ProductID)
+        , CONSTRAINT FK_OrderDetails_Products FOREIGN KEY (ProductID)
           REFERENCES dbo.Products (ProductID)
           ON DELETE CASCADE
           ON UPDATE CASCADE
-        , 
     )
 ;
