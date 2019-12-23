f23c8164209e4044fcb2ce6d2724c5d8a4d3bd56
Mateusz Gwozdz <37940021+Gwoozdz@users.noreply.github.com>
2019-12-23 13:07:02 +0100

    Update generator.py

--- generator/generator.py
+++ generator/generator.py
@@ -1,32 +1,35 @@
 import random
 
-VERBOSE = False
+_GEN_VERBOSE = False
 
 
-def file2list(filename):
-    if type(filename) is str:
-        return [line.strip() for line in open(filename, 'r')]
+def file2list(filename: str):
+    return [line.strip() for line in open(filename, 'r')]
 
 
-def generate(person_type):
+def generateNames(personel_type: chr, number: int):
+    """Generates names from files. Returns a list"""
 
-    if person_type == 'e':
-        # generate employees
-        employee_names = file2list(
+    if personel_type == 'e':
+        # gnerate employees
+        first_names = file2list(
             'first_names_f_e.txt') + file2list('first_names_m_e.txt')
         last_names = file2list('last_names.txt')
 
-        if VERBOSE:
-            print('names:', employee_names)
-        if VERBOSE:
+        if _GEN_VERBOSE:
+            print('first names:', first_names)
             print('last names:', last_names)
 
-        employees = [name + ' ' +
-                     random.choice(last_names) for name in employee_names]
-        if VERBOSE:
+        employees = [random.choice(first_names) + ' ' +
+                     random.choice(last_names) for _ in range(number)]
+
+        if _GEN_VERBOSE:
             print(employees)
+
         return employees
 
 
 # test
-print(generate('e'))
+emps = generateNames('e', 5)
+for e in emps:
+    print('(\'' + e + '\')')
