a3d90e781670c334ee5d2eaa1180bd7d9bfdd208
Mateusz Gwozdz <37940021+Gwoozdz@users.noreply.github.com>
2019-12-25 18:20:05 +0100

    Update CreateDB.sql
    add subcategories

--- CreateDB.sql
+++ CreateDB.sql
@@ -1,4 +1,5 @@
 /* CREATE DATABASE */
+
 USE master;
 GO
 
@@ -14,7 +15,10 @@ GO
 
 PRINT 'Database created successfully.'
 
+/* /CREATE DATABASE */
+--------------------------------------------------------------------
 /* CREATE TABLES */
+
 -- Customers
 CREATE TABLE [dbo].[Customers]
     ( 
@@ -27,11 +31,24 @@ CREATE TABLE [dbo].[Customers]
 -- Categories
 CREATE TABLE [dbo].[Categories]
     (
-        [CategoryID] INT IDENTITY(1,1) NOT NULL
+        [CategoryID] TINYINT NOT NULL
         , CONSTRAINT PK_Categories PRIMARY KEY (CategoryID)
-        , [CategoryName] NVARCHAR(20) NOT NULL
-        , [Description] NTEXT
+        , [Category Name] NVARCHAR(20) NOT NULL
     )   
+;
+
+-- Subcategories
+CREATE TABLE [dbo].[Subcategories]
+    (
+        [CategoryID] TINYINT NOT NULL
+        , CONSTRAINT [FK_Subcategories_Categories] FOREIGN KEY (CategoryID)
+          REFERENCES [dbo].[Categories] (CategoryID)
+          ON DELETE CASCADE
+          ON UPDATE CASCADE
+        , [SubcategoryID] TINYINT IDENTITY(1,1) NOT NULL UNIQUE
+        , [Subcategory Name] NVARCHAR(40) NOT NULL
+        , CONSTRAINT PK_Subcategories PRIMARY KEY (CategoryID, SubcategoryID)
+    )
 ;
 
 -- Products
@@ -40,7 +57,7 @@ CREATE TABLE [dbo].[Products]
         [ProductID] INT IDENTITY(1,1) NOT NULL
         , CONSTRAINT PK_Products PRIMARY KEY (ProductID) 
         , [ProductName] NVARCHAR(40) NOT NULL
-        , [CategoryID] INT
+        , [CategoryID] TINYINT
         , CONSTRAINT FK_Products_Categories FOREIGN KEY (CategoryID)
           REFERENCES dbo.Categories (CategoryID)
           ON DELETE CASCADE
@@ -216,42 +233,7 @@ IF @FillTables = 'true' BEGIN
 
     SET NOCOUNT ON
 
-    PRINT 'Inserting Departments...'
-    INSERT INTO [dbo].[Departments] VALUES
-      (1, 'Management')
-    , (2, 'IT')
-    , (3, 'Sales')
-    , (4, 'Packaging')
-    ;
-    PRINT 'Done.
-    '
-
-    PRINT 'Inserting Employees...'
-    INSERT INTO [dbo].[Employees] VALUES
-      ('Eryk Sorensen', 2)
-    , ('Elif Jensen', 3)
-    , ('Eliza Moller', 2)
-    , ('Elisabeth Christiansen', 1)
-    , ('Edward Larsen', 1)
-    , ('Emanuel Poulsen', 3)
-    , ('Eugeniusz Johansen', 3)
-    , ('Eryk Pedersen', 3)
-    , ('Elif Olsen', 2)
-    , ('Elzna Jorgensen', 1)
-    , ('Colin Petersen', 4)
-    , ('Carla Jorgensen', 4)
-    , ('Carmen Kristensen', 4)
-    , ('Cataleya Madsen', 4)
-    , ('Chiara Poulsen', 4)
-    , ('Chanel Thomsen', 4)
-    , ('Cyryl Larsen', 4)
-    , ('Chanell Jorgensen', 4)
-    , ('Cecylia Sorensen', 4)
-    , ('Chanell Nielsen', 4)
-    PRINT 'Done.
-    '
-
-    PRINT 'Inserting Customers...'
+    PRINT 'Initialising Customers...'
     INSERT INTO [dbo].[Customers]
     VALUES
       ('Celina Johansen')
@@ -265,6 +247,101 @@ IF @FillTables = 'true' BEGIN
     , ('Carla Christiansen')
     , ('Cyprian Moller')
     ;
+    PRINT 'Done.
+    '
+
+    PRINT 'Initialising Categories...'
+    INSERT INTO [dbo].[Categories] VALUES
+      (1, 'Twarz')
+    , (2, 'Usta')
+    , (3, 'Oczy')
+    , (4, 'Brwi')
+    , (5, 'Paznokcie')
+    , (6, 'Akcesoria')
+    , (7, 'Dermokosmetyki')
+    , (8, 'Zestawy')
+    ;
+    PRINT 'Done.
+    '
+
+    PRINT 'Initialising Subcategories...'
+    INSERT INTO [dbo].[Subcategories] VALUES
+    -- Twarz
+      (1, 'Podkłady')
+    , (1, 'Pudry')
+    , (1, 'Korektory')
+    , (1, 'Rozświetlacze i bronzery')
+    , (1, 'Róże')
+    , (1, 'Kremy BB i CC')
+    , (1, 'Bazy pod makijaż')
+    , (1, 'Spraye utrwalające makijaż')
+    , (1, 'Pędzle do makijażu')
+    , (1, 'Akcesoria')
+    -- Usta
+    , (2, 'Błyszczyki')
+    , (2, 'Pomadki do ust')
+    , (2, 'Konturówki do ust')
+    , (2, 'Pędzle do ust')
+    -- Oczy
+    , (3, 'Tusze do rzęs')
+    , (3, 'Bazy i cienie do powiek')
+    , (3, 'Paletki cieni')
+    , (3, 'Kredki do oczu i eyelinery')
+    , (3, 'Sztuczne rzęsy')
+    , (3, 'Odżywki do rzęs')
+    , (3, 'Pędzle do oczu')
+    , (3, 'Akcesoria')
+    -- Brwi
+    , (4, 'Kredki do brwi')
+    , (4, 'Cienie')
+    , (4, 'Korektory do brwi')
+    , (4, 'Maskary, żele, woski, pomady, henna')
+    , (4, 'Pędzle do brwi')
+    , (4, 'Akcesoria')
+    -- Paznokcie
+    , (5, 'Lakiery do paznokci')
+    , (5, 'Lakiery hybrydowe')
+    , (5, 'Pielęgnacja')
+    , (5, 'Zmywacze')
+    , (5, 'Zdobienie paznokci')
+    , (5, 'Bazy i utwardzanie, top coat')
+    , (5, 'Sztuczne paznokcie')
+    , (5, 'Akcesoria')
+    -- Akcesoria
+    , (6, 'Do twarzy')
+    , (6, 'Do ust')
+    , (6, 'Do oczu')
+    , (6, 'Do paznokci')
+    , (6, 'Pędzle do makijażu')
+    , (6, 'Kosmetyczki')
+    -- Dermokosmetyki nie mają podkategorii
+    -- Zestawy też    nie mają podkategorii
+    ;
+    PRINT 'Done.
+    '
+
+    PRINT 'Initialising Departments...'
+    INSERT INTO [dbo].[Departments] VALUES
+      (1, 'Management')
+    , (2, 'IT')
+    , (3, 'Sales')
+    , (4, 'Packaging')
+    ;
+    PRINT 'Done.
+    '
+
+    PRINT 'Initialising Employees...'
+    INSERT INTO [dbo].[Employees] VALUES
+      ('Eryk Sorensen', 4)
+    , ('Elif Jensen', 3)
+    , ('Eliza Moller', 2)
+    , ('Elisabeth Christiansen', 1)
+    , ('Edward Larsen', 1)
+    , ('Emanuel Poulsen', 3)
+    , ('Eugeniusz Johansen', 3)
+    , ('Eryk Pedersen', 4)
+    , ('Elif Olsen', 4)
+    , ('Elzna Jorgensen', 4)
     PRINT 'Done.
     '
 
