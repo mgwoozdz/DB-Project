7881b9e5676651ebb6c200b97a1b18f1a68e0a3f
Mateusz Gwozdz <37940021+Gwoozdz@users.noreply.github.com>
2020-01-16 01:17:12 +0100

    Fix trigger and files structure

--- TRIG_AssignBrandID.sql
+++ TRIG_AssignBrandID.sql
@@ -1,23 +1,6 @@
 USE TestDB;
 GO
 
-DROP TABLE IF EXISTS [dbo].[New Products];
-GO
-
--- temporary table for new products
-CREATE TABLE [dbo].[New Products]
-(
-    [Brand Name] NVARCHAR(20)
-    , [Product Name] NVARCHAR(40) NOT NULL
-    , [Product Description] NVARCHAR(100)
-    , [CategoryID] TINYINT
-    , [SubcategoryID] TINYINT
-    , [Wholesale Price] MONEY NOT NULL
-    , [Retail Price] MONEY NOT NULL
-)
-;
-GO
-
 DROP TRIGGER IF EXISTS [dbo].[TR_AssignBrandID];
 GO
 
@@ -43,11 +26,3 @@ BEGIN
 	JOIN [dbo].[Brands] AS B ON I.[Brand Name] = B.[Brand Name]
 
 END;
-
--- quick test
-INSERT INTO [dbo].[New Products] VALUES
-  ('Revlon', 'Colorstay', 'podkład z pompką do cery tłustej i mieszanej, 30 ml', 1, 1, 24, 48.99 )
-, ('Gosh', 'X-CEPTIONAL WEAR', 'kryjący podkład do twarzy w kremie do twarzy, 35 ml', 1, 1, 21, 41.99)
-
-SELECT * FROM [dbo].[Brands]
-SELECT * FROM [dbo].[Products]
