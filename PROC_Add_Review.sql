9c9ee418f2b4a7a332d5531aa0a15219c9b5311a
Mateusz Gwozdz <37940021+Gwoozdz@users.noreply.github.com>
2020-01-17 12:56:24 +0100

    Create PROC_Add_Review.sql

---
+++ PROC_Add_Review.sql
@@ -0,0 +1,30 @@
+IF OBJECT_ID('dbo.AddReview', 'P') IS NOT NULL
+DROP PROC dbo.AddReview
+GO
+
+CREATE PROC dbo.AddReview
+
+@CustomerID INT = NULL,
+@ProductID INT = NULL,
+@Content NTEXT = NULL,
+@Rating TINYINT = NULL
+
+AS
+
+	DECLARE @error AS NVARCHAR(400);
+	
+	IF @CustomerID IS NULL OR @ProductID IS NULL OR @Rating IS NULL
+	BEGIN
+		SET @error = 'Error! Rating invalid! Adding aborted.';
+		RAISERROR(@error, 16,1);
+		RETURN;
+	END
+
+	INSERT INTO dbo.[Reviews] VALUES
+	(@CustomerID, @ProductID, @Content, @Rating)
+
+
+GO
+
+EXEC dbo.AddReview 1, 1, NULL, 5
+GO
