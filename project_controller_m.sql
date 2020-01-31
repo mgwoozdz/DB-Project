106ca601b2c5c5841a6b872a7511d4c1448f0b37
Mateusz <37940021+11609@users.noreply.github.com>
2020-02-01 00:52:54 +0100

    Create project_controller_m.sql

---
+++ project_controller_m.sql
@@ -0,0 +1,24 @@
+
+-- create tables
+:r C:\Users\Mateusz\Desktop\DB-Project\CreateDB.sql
+GO
+
+-- new products trigg
+:r C:\Users\Mateusz\Desktop\DB-Project\TRIG_AssignBrandID.sql
+GO
+
+-- fill
+:r C:\Users\Mateusz\Desktop\DB-Project\FillDB.sql
+GO
+
+-- add customers
+:r C:\Users\Mateusz\Desktop\DB-Project\PROC_Add_Customer.sql
+GO
+
+SELECT * FROM [New Products]
+SELECT * FROM Products
+SELECT * FROM CUSTOMERS
+
+
+
+
