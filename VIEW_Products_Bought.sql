86f3efd4378dd9468f781d94dac64ab7cf5779ae
Mateusz <37940021+11609@users.noreply.github.com>
2020-02-01 01:32:30 +0100

    Update VIEW_Products_Bought.sql

--- VIEW_Products_Bought.sql
+++ VIEW_Products_Bought.sql
@@ -9,12 +9,12 @@ GO
 
 CREATE VIEW [Products Bought]
 AS
-SELECT P.[Product Name], Pr.[Quantity]
+SELECT B.[Brand Name] AS [Brand], P.[Product Name], Pr.[Quantity]
 FROM Products AS P
 JOIN (SELECT P.ProductID, SUM(OD.Quantity) AS [Quantity]
     FROM [dbo].[Products] AS P
     JOIN [dbo].[Order Details] AS OD ON P.ProductID = OD.ProductID
     GROUP BY P.ProductID) AS Pr ON P.ProductID = Pr.ProductID
+JOIN [dbo].[Brands] AS B ON P.BrandID = B.BrandID
 
 -- WORK IN PROGRESS
--- NOT TESTED TO WORK
