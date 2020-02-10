8bfb6e75d02927591ecd1a2cf5bf9a8b3deb42c0
Mateusz <37940021+11609@users.noreply.github.com>
2020-02-10 03:00:49 +0100

    Create FUNC_Product_Lookup.sql

---
+++ FUNC_Product_Lookup.sql
@@ -0,0 +1,19 @@
+USE TestDB
+GO
+
+DROP FUNCTION IF EXISTS [dbo].[Product Lookup]
+GO
+
+CREATE FUNCTION dbo.[Product Lookup] (@ProductName NVARCHAR(40) = '%')
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT Pr.[Product Name], Br.[Brand Name] AS [Brand], Ss.[Stock] AS [Availability]
+	FROM dbo.[Storage Status] AS Ss
+	JOIN dbo.[Products] AS Pr ON SS.[ProductID] = Pr.[ProductID]
+	JOIN dbo.[Brands] AS Br ON Pr.[BrandID] = Br.[BrandID]
+	WHERE (Ss.[Product Name] LIKE (@ProductName + '%')
+)
+;
+GO
