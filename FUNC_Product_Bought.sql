20576c99e27fbffd6c2211af7fc2cc3c4049ec88
Mateusz <37940021+11609@users.noreply.github.com>
2020-02-10 02:51:50 +0100

    Create FUNC_Product_Bought.sql

---
+++ FUNC_Product_Bought.sql
@@ -0,0 +1,17 @@
+USE TestDB
+GO
+
+DROP FUNCTION IF EXISTS [dbo].[Product Bought]
+GO
+
+CREATE FUNCTION dbo.[Product Bought] (@ProductName NVARCHAR(40) = '%')
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT *
+	FROM dbo.[Products Bought] AS P
+	WHERE (P.[Product Name] LIKE @ProductName)
+)
+;
+GO
