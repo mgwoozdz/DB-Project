67e429d9c1ee2892a8ff6ec7bd0727acee1eaca1
Mateusz <37940021+11609@users.noreply.github.com>
2020-02-11 21:41:31 +0100

    Update project_controller_m.sql

--- project_controller_m.sql
+++ project_controller_m.sql
@@ -1,32 +1,40 @@
--- create Tables
+-- create DB
+PRINT 'Creating Database...'
 :r C:\Users\Mateusz\Desktop\DB-Project\CreateDB.sql
 GO
+PRINT 'Done.'
 
--- Triggers
+
+-- add Triggers
 PRINT 'Deploying Triggers...'
 :r C:\Users\Mateusz\Desktop\DB-Project\TRIG_AssignBrandID.sql
 GO
 PRINT 'Done.'
 
--- add customers
-:r C:\Users\Mateusz\Desktop\DB-Project\PROC_Add_Customer.sql
-GO
 
--- add Buying functionality
+PRINT 'Deploying Procedures...'
+:r C:\Users\Mateusz\Desktop\DB-Project\PROC_Add_Customer.sql
 :r C:\Users\Mateusz\Desktop\DB-Project\PROC_Add_Review.sql
 :r C:\Users\Mateusz\Desktop\DB-Project\PROC_Add_To_Cart.sql
 :r C:\Users\Mateusz\Desktop\DB-Project\PROC_Buy.sql
 :r C:\Users\Mateusz\Desktop\DB-Project\PROC_Delete_From_Cart.sql
-:r C:\Users\Mateusz\Desktop\DB-Project\FUNC_Generate_Note.sql
 GO
+PRINT 'Done.'
 
--- Views
+
+PRINT 'Deploying Functions...'
+:r C:\Users\Mateusz\Desktop\DB-Project\FUNC_Generate_Note.sql
+:r C:\Users\Mateusz\Desktop\DB-Project\FUNC_Product_Bought.sql
+GO
+PRINT 'Done.'
+
+
 PRINT 'Deploying Views...'
 :r C:\Users\Mateusz\Desktop\DB-Project\VIEW_Products_Bought.sql --TESTED
 GO
 PRINT 'Done.'
 
--- fill
+
 PRINT 'Filling Tables...'
 :r C:\Users\Mateusz\Desktop\DB-Project\FillDB.sql
 GO
