39a7a4985624134eb4b4bec9f7351f11bc831fac
Mateusz <37940021+11609@users.noreply.github.com>
2020-02-11 22:32:30 +0100

    Create project_controller_template.sql

---
+++ project_controller_template.sql
@@ -0,0 +1,50 @@
+-- create DB
+PRINT 'Creating Database...'
+:r C:\Users\Mateusz\Desktop\DB-Project\CreateDB.sql
+GO
+PRINT 'Database created.
+	'
+
+	
+PRINT 'Deploying Views...'
+:r C:\Users\Mateusz\Desktop\DB-Project\VIEW_Products_Bought.sql
+:r C:\Users\Mateusz\Desktop\DB-Project\VIEW_Storage_Status.sql
+GO
+PRINT 'Views deployed.
+	'
+
+
+PRINT 'Deploying Triggers...'
+:r C:\Users\Mateusz\Desktop\DB-Project\TRIG_AssignBrandID.sql
+GO
+PRINT 'Triggers deployed.
+	'
+
+
+PRINT 'Deploying Procedures...'
+:r C:\Users\Mateusz\Desktop\DB-Project\PROC_Add_Customer.sql
+:r C:\Users\Mateusz\Desktop\DB-Project\PROC_Add_Review.sql
+:r C:\Users\Mateusz\Desktop\DB-Project\PROC_Add_To_Cart.sql
+:r C:\Users\Mateusz\Desktop\DB-Project\PROC_Buy.sql
+:r C:\Users\Mateusz\Desktop\DB-Project\PROC_Delete_From_Cart.sql
+GO
+PRINT 'Procedures deployed.
+	'
+
+
+PRINT 'Deploying Functions...'
+:r C:\Users\Mateusz\Desktop\DB-Project\FUNC_Generate_Note.sql
+:r C:\Users\Mateusz\Desktop\DB-Project\FUNC_Product_Bought.sql
+:r C:\Users\Mateusz\Desktop\DB-Project\FUNC_Product_Lookup.sql
+GO
+PRINT 'Functions deployed.
+	'
+
+
+
+
+PRINT 'Filling Tables...'
+:r C:\Users\Mateusz\Desktop\DB-Project\FillDB.sql
+GO
+PRINT 'Tables Filled.
+	'
