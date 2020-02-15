6525eae04e846ed3d2d2ab4056209bff014ed7a8
Mateusz <37940021+11609@users.noreply.github.com>
2020-02-16 00:59:04 +0100

    Update TRIG_Confirmation_Email.sql

---
+++ TRIG_Confirmation_Email.sql
@@ -0,0 +1,40 @@
+USE TestDB;
+GO
+
+DROP TRIGGER IF EXISTS [dbo].[Confirmation Email];
+GO
+
+CREATE TRIGGER [dbo].[Confirmation Email]
+ON [dbo].[Customers]
+AFTER INSERT
+AS
+IF (@@ROWCOUNT = 0)
+RETURN;
+	
+    DECLARE @TopicContent VARCHAR(50) = 'Witamy w Sklepie!'
+    DECLARE @BodyContent VARCHAR(MAX) = 
+    'Jeszcze tylko jeden krok i możesz cieszyć się zakupami!'
+    + 'Wystarczy że potwierdzisz swój adres email.'
+    + 'Pozdrawiamy,'
+    + 'Ekipa Twojego Sklepu'
+    
+    DECLARE c CURSOR
+    FOR SELECT [CustomerID] FROM INSERTED
+    WHERE [Mailing List] = 1 AND [Account Confirmed] = 0
+
+    DECLARE @cID INT
+
+    OPEN c
+    FETCH c INTO @cID
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        EXEC [dbo].[New Mail] @cID, @TopicContent, @BodyContent
+
+        FETCH c INTO @cID
+    END
+
+    CLOSE c
+    DEALLOCATE c
+
+
