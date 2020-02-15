562046c25b14a6a01a0547bb446fbc4a9edbe9ce
Mateusz <37940021+11609@users.noreply.github.com>
2020-02-15 17:20:38 +0100

    Update TRIG_New_Product_Notification.sql

--- TRIG_New_Product_Notification.sql
+++ TRIG_New_Product_Notification.sql
@@ -5,3 +5,9 @@ DROP TRIGGER IF EXISTS [dbo].[New Product Notification];
 GO
 
 CREATE TRIGGER [dbo].[New Product Notification]
+ON [dbo].[Products]
+AFTER INSERT
+AS
+IF (@@ROWCOUNT = 0)
+RETURN;
+	
