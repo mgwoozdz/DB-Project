4176c15346944c2e967173a2deb2a6547fa7f416
Mateusz <37940021+11609@users.noreply.github.com>
2020-02-17 13:42:13 +0100

    Update FUNC_Get_Reviews.sql

--- FUNC_Get_Reviews.sql
+++ FUNC_Get_Reviews.sql
@@ -5,8 +5,12 @@ DROP FUNCTION IF EXISTS [dbo].[Get Reviews];
 GO
 
 CREATE FUNCTION [dbo].[Get Reviews] (@ProductID INT)
-RETURNS TABLE(
-    SELECT * FROM [dbo].[Reviews]
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT [Content] AS [Review], [Rating]
+    FROM [dbo].[Reviews]
     WHERE ProductID = @ProductID
 )
 
