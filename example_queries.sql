f34ad08fa337dd855380f3dff9295b29429b3e9d
Mateusz Gwozdz <37940021+Gwoozdz@users.noreply.github.com>
2020-02-20 00:30:32 +0100

    Create example_queries.sql

---
+++ example_queries.sql
@@ -0,0 +1,8 @@
+-- proste wyszukiwanie produktu przez użytkownika
+EXEC [dbo].[Product Lookup] 'Oriflame'
+EXEC [dbo].[Product Lookup] 'Orif'
+EXEC [dbo].[Product Lookup] 'Av'
+EXEC [dbo].[Product Lookup] 'Revlon'
+
+-- dokładne wyszukiwanie
+EXEC [dbo].[Search]
